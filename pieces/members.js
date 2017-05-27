44a9fc27faa2bbb9b3139d5f7044bc9f7b78c069
Blizzardian <blizzardian94@gmail.com>
2017-05-27 21:07:17 +0000

    recommitting everything

---
+++ pieces/members.js
@@ -0,0 +1,65 @@
+var Discord     = require('discord.js')
+var Persistency = require.main.require('./persistency/members_persistency.js')
+var Piece       = require.main.require('./piece.js')
+
+class Members extends Piece {
+
+    key() {
+        return 'members'
+    }
+
+    description() {
+        return 'Blade and Soul external Clan Member handling'
+    }
+
+    initialize() {
+
+        /**
+         * Add member
+         */
+        this.addCommand('+|add [name]*', (data, context) => {
+            let name = data.name.join(' ')
+
+            if (Members.validateName(name, context)) {
+                Persistency.addMember(name, context)
+            }
+        }, {
+            description: 'Adds a member to the clan list.'
+        })
+
+        /**
+         * Remove member
+         */
+        this.addCommand('-|remove [name]*', (data, context) => {
+            let name = data.name.join(' ')
+
+            if (Members.validateName(name, context)) {
+                Persistency.removeMember(name, context)
+            }
+        }, {
+            description: 'Removes a member from the clan list.'
+        })
+
+        /**
+         * List member
+         */
+        this.addCommand('ls|list [delim]', (data, context) => {
+            let delim = data.delim
+
+            Persistency.listMember(context, delim)
+        }, {
+            description: 'Displays all members of the clan.'
+        })
+    }
+
+    static validateName(name, context) {
+        if (name !== null && name.length > 0) {
+            return true
+        } else {
+            context.message.channel.send('Invalid name.')
+            return false
+        }
+    }
+}
+
+module.exports = Members
